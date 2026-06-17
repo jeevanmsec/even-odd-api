9316309e2d83fce0886f823ba200cfa76243fae6
jeevanmsec <g.jeevan05@yahoo.co.in>
2026-06-17 11:03:24 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,15 +1,20 @@
 import unittest
-from app import is_even
+from app import app
 
-class TestEvenOdd(unittest.TestCase):
 
-    def test_even(self):
-        self.assertTrue(is_even(2))
-        self.assertTrue(is_even(10))
+class FlaskAppTests(unittest.TestCase):
 
-    def test_odd(self):
-        self.assertFalse(is_even(1))
-        self.assertFalse(is_even(5))
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_home_page_loads(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_page_contains_title(self):
+        response = self.client.get("/")
+        self.assertIn(b"Even or Odd Checker", response.data)
+
 
 if __name__ == "__main__":
     unittest.main()
